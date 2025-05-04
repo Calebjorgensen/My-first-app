f613352b15065f610e9443747c49716d0e8ba426
Calebjorgensen <121927129+Calebjorgensen@users.noreply.github.com>
2025-05-03 22:09:20 -0700

    week 2 lesson

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServerComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
